14d2beeebed43afbe54b3fdd6968c7fc491cfce4
venugopal <venugopl.reddy@onsite.co.in>
2020-03-09 16:28:12 +0530

    Full functionality

--- frontend/src/components/Todos/TodoItem.js
+++ frontend/src/components/Todos/TodoItem.js
@@ -1,30 +1,50 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {delTodo, toggleTodo} from '../../actions/Todos';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { delTodo, toggleTodo } from "../../actions/Todos";
 
 export class TodoItem extends Component {
-    onChange = (todo) => {
-        todo.is_completed = !todo.is_completed,
-        this.props.toggleTodo(todo.id, todo)
+  onChange = todo => {
+    (todo.is_completed = !todo.is_completed),
+      this.props.toggleTodo(todo.id, todo);
+  };
+  render() {
+    const todo = this.props.todoItem;
+    return (
+      <Fragment>
+        <li className="list-group-item" style={this.textStyle()}>
+          <input
+            type="checkbox"
+            style={{ "margin-right": "0.5em", transform: "scale(1.5)" }}
+            checked={todo.is_completed}
+            onChange={() => {
+              this.onChange(todo);
+            }}
+          />
+          {todo.name}
+          <button
+            className="btn btn-danger btn-sm float-right"
+            onClick={() => {
+              this.props.delTodo(todo.id);
+            }}
+          >
+            X
+          </button>
+        </li>
+      </Fragment>
+    );
+  }
+  textStyle = () => {
+    if (this.props.todoItem.is_completed) {
+      return {
+        textDecoration: "line-through",
+        color: "grey",
+        fontSize: "1.3em"
+      };
     }
-    render() {
-        const todo = this.props.todoItem
-        return (
-            <li className="list-group-item" style={this.textStyle()}>
-              <input type="checkbox" checked={todo.is_completed} onChange={() => {this.onChange(todo)}}/>
-              {todo.name}
-              <button className="btn btn-danger btn-sm float-right" onClick= {() => {this.props.delTodo(todo.id)}}>X</button>
-            </li>
-        )
-    }
-    textStyle = () => {
-        if(this.props.todoItem.is_completed){
-            return {
-                textDecoration: 'line-through',
-            }
-        }
-    }
+    return {
+      fontSize: "1.5em"
+    };
+  };
 }
 
-
-export default connect(null, {delTodo, toggleTodo})(TodoItem);
+export default connect(null, { delTodo, toggleTodo })(TodoItem);
